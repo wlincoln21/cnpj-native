e7787c10a828e0b0a37cf06103ae8b286502c260
Wesley Lincoln <wesleylincoln14@outlook.com>
2023-09-07 23:34:50 -0300

    first commit

---
+++ App.tsx
@@ -0,0 +1,15 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React, { Component } from 'react'
+
+export class App extends Component {
+  render() {
+    return (
+      <View>
+        <Text>App</Text>
+      </View>
+    )
+  }
+}
+
+
+export default App
